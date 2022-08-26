a1e74f03d136dbcd3b1a4fd4d05bb3f0aad139d7
Wissem-Zidi <wissem.zidi.ofc@gmail.com>
2022-08-26 21:20:53 +0100

    fixing key press in mobile

--- main.js
+++ main.js
@@ -2,9 +2,8 @@
 const emailInput = document.querySelector("#email__input");
 const passwordInput = document.querySelector("#password__input");
 const submitBtn = document.querySelector("#login__submit__btn");
-const specialCharacters = "&#|/)([]{}=+.,:/!§*µ$£!;?-_~²`";
 
-emailInput.addEventListener("keydown", () => {
+emailInput.addEventListener("keyup", () => {
   if (!emailInput.value.includes("@") || !emailInput.value.includes(".")) {
     emailInput.className = "invalid";
   } else {
@@ -12,7 +11,7 @@ emailInput.addEventListener("keydown", () => {
   }
 });
 
-passwordInput.addEventListener("keydown", () => {
+passwordInput.addEventListener("keyup", () => {
   if (passwordInput.value.length >= 6) {
     passwordInput.className = "valid";
   } else {
