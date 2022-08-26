4829bcf7c5f4d491ca1725abadf4f9d27869d3ea
Wissem-Zidi <wissem.zidi.ofc@gmail.com>
2022-08-26 22:05:04 +0100

    Main page

--- main.js
+++ main.js
@@ -18,3 +18,44 @@ passwordInput.addEventListener("keyup", () => {
     passwordInput.className = "invalid";
   }
 });
+
+submitBtn.addEventListener("click", () => {
+  if (!emailInput.value.includes("@") || !emailInput.value.includes(".")) {
+    emailInput.className = "invalid";
+    emailInput.animate(
+      [
+        { transform: "translateX(0px)" },
+        { transform: "translateX(-5px)" },
+        { transform: "translateX(5px)" },
+        { transform: "translateX(0px)" },
+      ],
+      {
+        duration: 200,
+      }
+    );
+    setTimeout(() => {
+      emailInput.className = "";
+    }, 2000);
+  } else {
+    emailInput.className = "valid";
+  }
+  if (passwordInput.value.length >= 6) {
+    passwordInput.className = "valid";
+  } else {
+    passwordInput.className = "invalid";
+    passwordInput.animate(
+      [
+        { transform: "translateX(0px)" },
+        { transform: "translateX(-5px)" },
+        { transform: "translateX(5px)" },
+        { transform: "translateX(0px)" },
+      ],
+      {
+        duration: 200,
+      }
+    );
+    setTimeout(() => {
+      passwordInput.className = "";
+    }, 2000);
+  }
+});
